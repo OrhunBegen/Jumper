9ecb8a5450c990ba8fc7e09cb722490ea5e5b4a8
OrhunBegen <orhunbegen@outlook.com>
2023-12-11 20:19:53 +0300

    last fkn working thing

--- Source/Jumper/MoverPlatform.cpp
+++ Source/Jumper/MoverPlatform.cpp
@@ -6,41 +6,58 @@
 // Sets default values
 AMoverPlatform::AMoverPlatform()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
 }
 
 // Called when the game starts or when spawned
 void AMoverPlatform::BeginPlay()
 {
 	Super::BeginPlay();
+
 	StartLocation = GetActorLocation();
-	UE_LOG(LogTemp, Display, TEXT("Your text"));
-	UE_LOG(LogTemp, Warning, TEXT("Your text"));
-	UE_LOG(LogTemp, Error, TEXT("Your text"));
 }
 
 // Called every frame
 void AMoverPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	//Move Platform forwards
-	//Get Current Location
-	FVector CurrentLocation = GetActorLocation();	
-	//Add vector to the that location
-	CurrentLocation = (PlatformVelocity*DeltaTime) + CurrentLocation;
-	//Set new location
-	SetActorLocation(CurrentLocation);
-	//Send Platform back if gone too far
-	//Check how far it has gone
-	const float MovedDistance = FVector::Dist(StartLocation, CurrentLocation);
-	//Reverse direction of motion if it s gone too far
-	if (MovedDistance > MovebleDistance)
+
+	MovePlatform(DeltaTime);
+	RotatePlatform(DeltaTime);
+}
+
+
+void AMoverPlatform::MovePlatform(float DeltaTime)
+{
+	if (ShouldPlatformReturn())
 	{
-		const FVector	MoveDirection = PlatformVelocity.GetSafeNormal();
-		StartLocation = StartLocation + (MoveDirection *MovebleDistance);
 		SetActorLocation(StartLocation);
 		PlatformVelocity = -PlatformVelocity;
 	}
+	else
+	{
+		FVector CurrentLocation = GetActorLocation();
+		CurrentLocation = CurrentLocation + (PlatformVelocity * DeltaTime);
+		SetActorLocation(CurrentLocation);
+	}
 }
 
+void AMoverPlatform::RotatePlatform(float DeltaTime)
+{
+	FRotator CurrentRotation = GetActorRotation();
+	CurrentRotation = CurrentRotation + RotationVelocity * DeltaTime;
+	SetActorRotation(CurrentRotation);
+}
+
+
+bool AMoverPlatform::ShouldPlatformReturn() const
+{
+	return GetDistanceMoved() > MoveDistance;
+}
+
+float AMoverPlatform::GetDistanceMoved() const
+{
+	return FVector::Dist(StartLocation, GetActorLocation());
+}
--- Source/Jumper/MoverPlatform.h
+++ Source/Jumper/MoverPlatform.h
@@ -23,12 +23,22 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(EditAnywhere, Category="Moving Plaform")
-	FVector PlatformVelocity = FVector(50, 0, 0);
+private:
+
+	UPROPERTY(EditAnywhere, Category="Moving")
+	FVector PlatformVelocity = FVector(100, 0, 0);
+	UPROPERTY(EditAnywhere, Category="Moving")
+	float MoveDistance = 100;
+	UPROPERTY(EditAnywhere, Category="Rotation")
+	FRotator RotationVelocity;
+
 	FVector StartLocation;
 
-	UPROPERTY(EditAnywhere, Category="Moving Platform")
-	float MovebleDistance = 100;
+	void MovePlatform(float DeltaTime);
+	void RotatePlatform(float DeltaTime);
 
-	
+	bool ShouldPlatformReturn() const;
+	float GetDistanceMoved() const;
+
 };
+
